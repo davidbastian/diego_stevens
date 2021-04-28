206b4372f14525d52b1bec9a5c18e9465bc903a4
davidbastian <d@davidbastian.red>
2021-04-28 18:11:18 +0200

    added about images css

--- app/src/views/home/view.js
+++ app/src/views/home/view.js
@@ -38,20 +38,79 @@ class View {
                 </section>
 
                 <section id="about" class="border">
-                    <div class="introduction">
+                    <div class="about-introduction content">
                         <p>${self.data.about.introduction}</p>
                     </div>
 
                     <figure class="quote">
                         <blockquote>
-                        The art of making <br>
-                        technology <br>
-                        more natural.
+                        ${self.data.about.quotes[0].quote}
                         </blockquote>
-                        <figcaption> Keynote Conference, <cite>2020</cite> </figcaption>
+                        <figcaption> ${self.data.about.quotes[0].caption}, <cite>${self.data.about.quotes[0].year}</cite> </figcaption>
                     </figure>
 
-                
+                    <div class="content about-moments">
+                        
+                    <figure class="vertical" style="margin-left: 6vw;">
+                        <img style="width:27vw" src="common/media/img/001.png" alt="">
+                        <dl>
+                            <dt>Diego & Lorem ipsum</dt>
+                            <dd>Photo by Peter Stackpole <br> Photograph by Ronald Dick</dd>
+                        </dl>
+                    </figure>
+
+                    <figure class="vertical" style="margin-top: -25vh;  margin-left: 46vw;">
+                        <img  style="width: 27vw;" src="common/media/img/002.png" alt="">
+                        <dl>
+                            <dt>Diego & Lorem ipsum</dt>
+                            <dd>Photo by Peter Stackpole <br> Photograph by Ronald Dick</dd>
+                        </dl>
+                    </figure>
+
+                    <figure class="vertical" style="margin-top: -21vh;">
+                        <img style="width: 27vw;"  src="common/media/img/003.png" alt="">
+                        <dl>
+                            <dt>Diego & Lorem ipsum</dt>
+                            <dd>Photo by Peter Stackpole <br> Photograph by Ronald Dick</dd>
+                        </dl>
+                    </figure>
+
+                    <figure class="horizontal"  style="margin-left: 40vw;margin-top: -8vh;">
+                        <img style="width:20vw;" src="common/media/img/004.png" alt="">
+                        <dl>
+                            <dt>Diego & Lorem ipsum</dt>
+                            <dd>Photo by Peter Stackpole <br> Photograph by Ronald Dick</dd>
+                        </dl>
+                    </figure>
+                    </div>
+
+                    <figure class="quote">
+                        <blockquote>
+                        ${self.data.about.quotes[1].quote}
+                        </blockquote>
+                        <figcaption> ${self.data.about.quotes[1].caption}, <cite>${self.data.about.quotes[1].year}</cite> </figcaption>
+                    </figure>
+
+                    <div class="content about-moments">
+                        
+                    <figure class="vertical" style="margin-left: 6vw;">
+                        <img style="width:27vw" src="common/media/img/005.png" alt="">
+                        <dl>
+                            <dt>Diego & Lorem ipsum</dt>
+                            <dd>Photo by Peter Stackpole <br> Photograph by Ronald Dick</dd>
+                        </dl>
+                    </figure>
+
+                    <figure class="vertical" style="margin-top: -25vh;  margin-left: 46vw;">
+                        <img  style="width: 27vw;" src="common/media/img/006.png" alt="">
+                        <dl>
+                            <dt>Diego & Lorem ipsum</dt>
+                            <dd>Photo by Peter Stackpole <br> Photograph by Ronald Dick</dd>
+                        </dl>
+                    </figure>
+
+                    
+                    </div>
                 </section>
         `
         document.body.querySelector('main').insertAdjacentHTML('afterbegin', markup);
